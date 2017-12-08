d1fcb1776330db89cbdc5f0355718ab3dfe5dcb3
JP Norair <jpnorair@indigresso.com>
2017-12-08 10:10:38 -0800

    makefiles

---
+++ otlib/otlib.mk
@@ -0,0 +1,64 @@
+CC=gcc
+
+TARGET      := otlib
+OTFS_DEF    ?= 
+OTFS_INC    ?=
+OTFS_LIB    ?= 
+
+#CFLAGS      := -std=gnu99 -O -g -Wall
+CFLAGS      := -std=gnu99 -O3
+
+BUILDDIR    := ../build/$(TARGET)
+TARGETDIR   := .
+SRCEXT      := c
+DEPEXT      := d
+OBJEXT      := o
+LIB         := $(OTFS_LIB)
+INC         := $(patsubst -I./%, -I./../%, $(OTFS_INC)) 
+INCDEP      := $(INC)
+
+SOURCES     := $(shell find . -type f -name "*.$(SRCEXT)")
+OBJECTS     := $(patsubst ./%,$(BUILDDIR)/%,$(SOURCES:.$(SRCEXT)=.$(OBJEXT)))
+
+
+all: resources $(TARGET)
+obj: $(OBJECTS)
+remake: cleaner all
+
+
+#Copy Resources from Resources Directory to Target Directory
+resources: directories
+
+#Make the Directories
+directories:
+	@mkdir -p $(TARGETDIR)
+	@mkdir -p $(BUILDDIR)
+
+#Clean only Objects
+clean:
+	@$(RM) -rf $(BUILDDIR)
+
+#Full Clean, Objects and Binaries
+cleaner: clean
+	@$(RM) -rf $(TARGETDIR)
+
+#Pull in dependency info for *existing* .o files
+-include $(OBJECTS:.$(OBJEXT)=.$(DEPEXT))
+
+#Direct build of the test app with objects
+$(TARGET): $(OBJECTS)
+	$(CC) -o $(TARGETDIR)/$(TARGET) $^ $(LIB)
+
+#Compile Stages
+$(BUILDDIR)/%.$(OBJEXT): ./%.$(SRCEXT)
+	@mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) $(OTFS_DEF) $(INC) -c -o $@ $<
+	@$(CC) $(CFLAGS) $(OTFS_DEF) $(INCDEP) -MM ./$*.$(SRCEXT) > $(BUILDDIR)/$*.$(DEPEXT)
+	@cp -f $(BUILDDIR)/$*.$(DEPEXT) $(BUILDDIR)/$*.$(DEPEXT).tmp
+	@sed -e 's|.*:|$(BUILDDIR)/$*.$(OBJEXT):|' < $(BUILDDIR)/$*.$(DEPEXT).tmp > $(BUILDDIR)/$*.$(DEPEXT)
+	@sed -e 's/.*://' -e 's/\\$$//' < $(BUILDDIR)/$*.$(DEPEXT).tmp | fmt -1 | sed -e 's/^ *//' -e 's/$$/:/' >> $(BUILDDIR)/$*.$(DEPEXT)
+	@rm -f $(BUILDDIR)/$*.$(DEPEXT).tmp
+
+#Non-File Targets
+.PHONY: all remake clean cleaner resources
+
